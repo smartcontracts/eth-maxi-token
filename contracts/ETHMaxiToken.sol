b637cbc6d00942a7f64ed16ce9d9619549f6c1fe
Kelvin Fichter <kelvinfichter@gmail.com>
2021-03-20 13:51:13 -0700

    Doing some reworking

--- contracts/ETHMaxiToken.sol
+++ contracts/ETHMaxiToken.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.5.0 <0.8.0;
 
+/* Library Imports */
 import { Lib_RLPReader } from "./libraries/Lib_RLPReader.sol";
 import { Lib_SecureMerkleTrie } from "./libraries/Lib_SecureMerkleTrie.sol";
+import { Lib_EIP155Tx } from "./libraries/Lib_EIP155Tx.sol";
 
 contract ETHMaxiToken {
+    using Lib_EIP155Tx for Lib_EIP155Tx.EIP155Tx;
+
     event Transfer(
         address indexed _from,
         address indexed _to,
@@ -14,6 +18,17 @@ contract ETHMaxiToken {
     event Approval(
         address indexed _owner,
         address indexed _spender,
+        uint256 _value
+    );
+
+    event Claimed(
+        address indexed _owner,
+        uint256 _value
+    );
+
+    event Slashed(
+        address indexed _owner,
+        address indexed _slasher,
         uint256 _value
     );
 
@@ -40,16 +55,36 @@ contract ETHMaxiToken {
     uint256 public lockupEndTime;
 
     // Hashes that people will use to prove their balances.
+    uint256 public snapshotBlockNumber;
     bytes32 public snapshotBlockHash;
     bytes32 public snapshotStateRoot;
 
     constructor(
-        uint256 _lockupPeriod
+        uint256 _lockupPeriod,
+        uint256 _snapshotBlockNumber,
+        bytes memory _snapshotBlockHeader
     ) {
         lockupEndTime = block.timestamp + _lockupPeriod;
-        snapshotBlockHash = blockhash(block.number - 1);
+        snapshotBlockNumber = _snapshotBlockNumber;
+        snapshotBlockHash = blockhash(_snapshotBlockNumber);
+
+        // Just a safety measure.
+        require(
+            keccak256(_snapshotBlockHeader) == snapshotBlockHash,
+            "ETHMaxiToken: block header does not match snapshot block hash"
+        );
+
+        // Decode the block header in order to pull out the state root.
+        Lib_RLPReader.RLPItem[] memory blockHeader = Lib_RLPReader.readList(
+            _snapshotBlockHeader
+        );
+        snapshotStateRoot = Lib_RLPReader.readBytes32(blockHeader[3]);
     }
 
+    /**
+     * We use a lockup period to prevent people from claiming and then transferring their tokens to
+     * avoid getting slashed. Simple modifier for checking this condition.
+     */
     modifier onlyAfterLockup() {
         require(
             block.timestamp > lockupEndTime,
@@ -58,28 +93,16 @@ contract ETHMaxiToken {
         _;
     }
 
-    function setStateRoot(
-        bytes memory _encodedBlockHeader
-    )
-        public
-    {
-        require(
-            snapshotStateRoot == bytes32(0),
-            "ETHMaxiToken: state root has already been set"
-        );
-
-        require(
-            keccak256(_encodedBlockHeader) == snapshotBlockHash,
-            "ETHMaxiToken: block header does not match snapshot block hash"
-        );
-
-        Lib_RLPReader.RLPItem[] memory blockHeader = Lib_RLPReader.readList(
-            _encodedBlockHeader
-        );
-
-        snapshotStateRoot = Lib_RLPReader.readBytes32(blockHeader[3]);
-    }
-
+    /**
+     * Function for redeeming tokens at a 1:1 ratio to ETH at the snapshot block. If you had 1 ETH
+     * (= 10^18 wei) at the snapshot, you have 10^18 tokens. I.e., you have the same amount of
+     * tokens. Also allows you to claim on behalf of someone else (*you* do the proof, *they* get
+     * the money). Perhaps useful if you want to quickly claim and slash.
+     * @param _owner Address to claim tokens for.
+     * @param _proof RLP-encoded merkle trie inclusion proof for the address's account at the
+     *               snapshot block height.
+     * @return `true` if the function succeeded.
+     */
     function claim(
         address _owner,
         bytes memory _proof
@@ -89,11 +112,13 @@ contract ETHMaxiToken {
             bool
         )
     {
+        // You can only claim once per address.
         require(
             claimed[_owner] == false,
             "ETHMaxiToken: balance for address has already been claimed"
         );
 
+        // Pull out the encoded account from the merkle trie proof.
         (bool exists, bytes memory encodedAccount) = Lib_SecureMerkleTrie.get(
             abi.encodePacked(_owner),
             _proof,
@@ -105,46 +130,43 @@ contract ETHMaxiToken {
             "ETHMaxiToken: bad eth merkle proof"
         );
 
+        // Decode account to get its balance.
         Lib_RLPReader.RLPItem[] memory account = Lib_RLPReader.readList(
             encodedAccount
         );
-
         uint256 amount = Lib_RLPReader.readUint256(account[1]);
 
+        // Mark as claimed and give out the balance.
         claimed[_owner] = true;
         balances[_owner] = amount;
+
         emit Transfer(address(0), _owner, amount);
+        emit Claimed(_owner, amount);
         return true;
     }
 
+    /**
+     * Slashes an account based on a signed EIP155 transaction with a chain ID other than 1. Simply
+     * provide the encoded signed transaction and be rewarded with the heretic's entire (claimed)
+     * balance! Will *not* work if the user you're slashing hasn't claimed a balance yet. But you
+     * can also claim on behalf of other users if you want to do some slashin'.
+     * @param _encodedEIP155Tx RLP-encoded signed EIP155 transaction.
+     * @return `true` if the slashin' was successful.
+     */
     function slash(
-        bytes memory _encodedUnsignedTransaction,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
+        bytes memory _encodedEIP155Tx
     )
         public
         returns (
             bool
         )
     {
-        Lib_RLPReader.RLPItem[] memory transaction = Lib_RLPReader.readList(
-            _encodedUnsignedTransaction
+        Lib_EIP155Tx.EIP155Tx memory transaction = Lib_EIP155Tx.decode(
+            _encodedEIP155Tx,
+            1 // chain id of ethereum
         );
 
-        uint256 chainId = Lib_RLPReader.readUint256(transaction[6]);
-
-        require(
-            chainId != 1,
-            "ETHMaxiToken: chain id cannot be 1 (reserved for ethereum)"
-        );
-
-        address owner = ecrecover(
-            keccak256(_encodedUnsignedTransaction),
-            uint8(_v - 2 * chainId - 35),
-            _r,
-            _s
-        );
+        address owner = transaction.sender();
 
         require(
             claimed[owner] == true,
@@ -156,9 +178,13 @@ contract ETHMaxiToken {
             "ETHMaxiToken: address has already been slashed"
         );
 
+        uint256 amount = balances[owner];
+
         slashed[owner] = true;
-        balances[msg.sender] += balances[owner];
+        balances[msg.sender] += amount;
         balances[owner] = 0;
+        emit Transfer(owner, msg.sender, amount);
+        emit Slashed(owner, msg.sender, amount);
         return true;
     }
 
